f870f941aacb6f4a3600d8bfd222f3e717d7322c
saurabh <saurabh@jobyfy.com>
2019-01-28 20:20:33 +0530

    change poison version for phoenix app

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Bitcoinmonitor.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:poison, "~> 3.1.0"},
+      {:poison, "~> 2.0"},
       {:websockex, ">= 0.0.0"},
       {:timex, "~> 3.1"}
     ]
